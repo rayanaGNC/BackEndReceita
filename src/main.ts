ade6d5c86204c817ef7ea49bf14d297b11ed09df
rayanaGNC <rgnconrat@minha.fag.edu.br>
2025-06-11 19:35:41 -0300

    amem

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  
+
+  const port = process.env.PORT ?? 3000;
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
